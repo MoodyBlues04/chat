51f0139d8a29fee1e7929a5e2bccf42b9798ad81
Anton Sokolov <sokant2005@gmail.com>
2022-07-12 22:39:34 +0400

    .

--- controllers/TmpController.php
+++ controllers/TmpController.php
@@ -7,10 +7,6 @@ use yii\web\Controller;
 class TmpController extends Controller {
 
     public function actionSay() {
-        echo "<pre>";
-        for ($i = 0; $i < 100; $i++) {
-            echo "hi" * 100 . PHP_EOL;
-        }
-        echo "</pre>";
+
     }
 }
