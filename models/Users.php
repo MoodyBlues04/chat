49e99f63d1bbc0910e17b9cb5189a1fdefe3542a
Anton Sokolov <sokant2005@gmail.com>
2022-07-14 01:03:17 +0400

    .

--- models/Users.php
+++ models/Users.php
@@ -6,7 +6,7 @@ use yii\db\ActiveRecord;
 
 class Users extends ActiveRecord {
     
-    public function rules(){
+    public function rules() {
         return [
             [['name', 'password'], 'required', 'message' => 'please fill in all fields'],
             [ ['password'], 'string', 'min' => 8],
