892e84dc7b14d779fd1ac14f77b81d9af27c3f88
Anton Sokolov <sokant2005@gmail.com>
2022-07-17 13:04:42 +0400

    some

--- views/index/logIn.php
+++ views/index/logIn.php
@@ -12,7 +12,7 @@ AppAsset::register($this);
 <h1 style="margin-bottom: 20px"><?= Html::encode($this->title) ?></h1>
 
 <?php $form = ActiveForm::begin(); ?>
-    <?= $form->field($model, 'name', ['enableLabel' => false])->textInput(['placeholder' => 'Name', 'class'=>'form-control text-center']) ?>
+    <?= $form->field($model, 'username', ['enableLabel' => false])->textInput(['placeholder' => 'Name', 'class'=>'form-control text-center']) ?>
     <?= $form->field($model, 'password', ['enableLabel' => false])->passwordInput(['placeholder' => 'Password', 'class'=>'form-control text-center']) ?>
     <?= Html::submitButton('Login', ['class' => 'login-button btn btn-primary']) ?>
 <?php ActiveForm::end(); ?>
