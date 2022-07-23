f57b385c6e8706dd5a168e83c774933398acc647
Anton Sokolov <sokant2005@gmail.com>
2022-07-23 16:51:58 +0400

    front

--- views/layouts/home.php
+++ views/layouts/home.php
@@ -9,6 +9,7 @@ use yii\bootstrap4\Breadcrumbs;
 use yii\bootstrap4\Html;
 use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
+use yii\bootstrap4\ActiveForm;
 
 AppAsset::register($this);
 ?>
@@ -27,15 +28,38 @@ AppAsset::register($this);
     <body>
     <?php $this->beginBody() ?>
         <header>
-            t
+            <div class="search">
+                there is search line (for users)
+            </div>
+            
+            <div class="user">
+            <?php
+            // устанавливаем первый и последний год диапазона
+            $yearArray = range(2000, 2005);
+            ?>
+            <!-- выводим выпадающий список -->
+            <select name="year">
+                <option value="">Выберите год</option>
+                <?php
+                foreach ($yearArray as $year) {
+                    // если вы хотите выбрать конкретный год
+                    $selected = ($year == 2015) ? 'selected' : '';
+                    echo '<option '.$selected.' value="'.$year.'">'.$year.'</option>';
+                }
+                ?>
+            </select>            
+                user icon & drop down list
+            </div>
         </header>
         <main>
             <div class="links">
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
+                <a href="" class="link">main</a>
+                <a href="#1" class="link">feature 1</a>
+                <a href="#2" class="link">feature 2</a>
+                <a href="#3" class="link">feature 3</a>
+                <a href="#4" class="link">feature 4</a>
+                <a href="#5" class="link">feature 5</a>
+                <a href="#6" class="link">feature 6</a>
             </div>
             <div class="content">
                 <?= Alert::widget() ?>
